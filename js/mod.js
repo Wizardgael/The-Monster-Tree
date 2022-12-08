7496cde6cf5310fe6cffd0768c3746bd99ac17d4
Gaël <gael.bachelier@gmail.com>
2022-12-08 16:10:55 +0100

    ini

--- js/mod.js
+++ js/mod.js
@@ -1,14 +1,14 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Monster Tree",
+	id: "monster-tree",
+	author: "Eowondil",
+	pointsName: "monster essences",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal(10), // Used for hard resets and new players
+	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
@@ -28,18 +28,18 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){
-    return new Decimal(modInfo.initialStartPoints)
+function getStartPoints() {
+	return new Decimal(modInfo.initialStartPoints)
 }
 
 // Determines if it should show points/sec
-function canGenPoints(){
+function canGenPoints() {
 	return true
 }
 
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints())
+	if (!canGenPoints())
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
@@ -47,8 +47,10 @@ function getPointGen() {
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+function addedPlayerData() {
+	return {
+	}
+}
 
 // Display extra things at the top of the page
 var displayThings = [
@@ -70,10 +72,10 @@ var backgroundStyle = {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return (3600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function fixOldSave(oldVersion) {
 }
